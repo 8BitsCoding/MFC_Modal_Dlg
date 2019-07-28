852e14640e5799a33d780ebf8a0daa187faea8af
taehyeong77@naver.com <taehyeong77@naver.com>
2019-07-28 11:20:53 +0900

    commit

---
+++ MFCApplication1/newDlg.cpp
@@ -0,0 +1,63 @@
+﻿// newDlg.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+#include "MFCApplication1.h"
+#include "newDlg.h"
+#include "afxdialogex.h"
+
+
+// newDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(newDlg, CDialogEx)
+
+newDlg::newDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_NEW_DLG, pParent)
+{
+
+}
+
+newDlg::~newDlg()
+{
+}
+
+void newDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(newDlg, CDialogEx)
+	ON_BN_CLICKED(IDOK, &newDlg::OnBnClickedOk)
+	ON_BN_CLICKED(IDC_EXIT_BTN, &newDlg::OnBnClickedExitBtn)
+END_MESSAGE_MAP()
+
+
+// newDlg 메시지 처리기
+
+
+BOOL newDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+
+	SetDlgItemInt(IDC_CH_NUM_EDIT, m_num);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+
+void newDlg::OnBnClickedOk()
+{
+	m_num = GetDlgItemInt(IDC_CH_NUM_EDIT);
+
+	CDialogEx::OnOK();
+}
+
+
+void newDlg::OnBnClickedExitBtn()
+{
+	EndDialog(20);
+}
---
+++ MFCApplication1/newDlg.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+
+// newDlg 대화 상자
+
+class newDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(newDlg)
+
+public:
+	newDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~newDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_NEW_DLG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+
+private:
+	int m_num;
+
+public:
+	void SetNum(int a_num) {
+		m_num = a_num;
+	}
+	int GetNum() {
+		return m_num;
+	}
+	virtual BOOL OnInitDialog();
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedExitBtn();
+};
